99727f5bc339d90e818b297362eac37dc3206d09
Ferum2704 <60216723+Ferum2704@users.noreply.github.com>
2021-06-01 10:53:27 +0300

    Add files via upload

---
+++ Classes/AccountHandler.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LibraryWallet
+{
+    public delegate void AccountHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }
+        public AccountEventArgs(string someMessage)
+        {
+            Message = someMessage;
+        }
+    }
+}
---
+++ Classes/DepositAccount.cs
@@ -0,0 +1,37 @@
+﻿
+namespace LibraryWallet
+{
+    public class DepositAccount : Account
+    {
+        double interest = 0;
+        int interestPeriod;
+        public event AccountHandler ChargedInterest;
+        public DepositAccount(string someName, int someId, decimal initialBalance):base(someName, someId, initialBalance)
+        {
+            interestPeriod = 0;
+        }
+        private void OnCharged(AccountEventArgs e)
+        {
+            ChargedInterest(this, e);
+        }
+        public void SetInterest(double someInterest)
+        {
+            interest = someInterest;
+        }
+        public override void AddMoney(decimal someSum)
+        {
+            base.AddMoney(someSum);
+            interestPeriod += 15;
+            if (interestPeriod % 30 == 0)
+            {
+                CalculateBalanceWithInterest();
+            }
+        }
+        private void CalculateBalanceWithInterest()
+        {
+            decimal depositIncome = Balance * (decimal)interest;
+            Balance += depositIncome;
+            OnCharged(new AccountEventArgs($"\nThe {Name} charged interests in the amount of {depositIncome}!"));
+        }
+    }
+}
