bc59bfd5983791f74578a8dbd84df905cc75d3a5
Ferum2704 <60216723+Ferum2704@users.noreply.github.com>
2021-06-01 10:53:57 +0300

    Add files via upload

--- Classes/MyException.cs
+++ Classes/MyException.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 
-namespace Classes
+namespace LibraryWallet
 {
     public class OperationPerformingException : Exception
     {
         public OperationPerformingException(string str) : base(str) { }
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+    public class FileEmptyException : Exception
+    {
+        public FileEmptyException(string str) : base(str) { }
         public override string ToString()
         {
             return Message;
